113eaca5fe736b9edb11fb358912b2382395875e
Connor Young <c_young97@live.com>
2023-08-10 20:06:16 +1000

    Chunk generation
    Currently working on generating chucks. 
    I have a tile map that I am trying to spawn different objects on. Waste of time. Need to focus on the Generation.

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Chunk : MonoBehaviour
+{
+    [SerializeField] private Tilemap mMap;
+    [SerializeField] public List<GameObject> mProps = new List<GameObject>();
+    [SerializeField] private int mMaxProps = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mMap == null)
+        {
+            Debug.Log("Missing tile map on chunk");
+            return;
+        }
+        if (mProps.Count == 0)
+        {
+            Debug.Log("Missing props on chunk");
+            return;
+        }
+    }
+}
